6a5a7bcaf3d06549319cebbe6679d1a48f98ceda
izhar <izharnjoroge@gmail.com>
2024-05-29 18:48:29 +0300

    added max width

--- src/components/reusables/container.tsx
+++ src/components/reusables/container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="w-auto mx-auto bg-white min-h-screen flex flex-col border-l border-r">
+    <div className="max-w-[1200px] mx-auto bg-white min-h-screen flex flex-col">
       {children}
     </div>
   );

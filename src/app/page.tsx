0d25fad048d75a05fc3d803afb9d80f20dc285c8
izhar <izharnjoroge@gmail.com>
2024-10-07 13:21:42 +0300

    removed conditional useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useAuthContext } from "../context/AuthContext";
-import Spinner from "../components/reusables/spinner";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { loading } = useAuthContext();
   const router = useRouter();
-
-  if (loading) {
-    return <Spinner />;
-  }
 
   useEffect(() => {
     router.replace("/myShop");
